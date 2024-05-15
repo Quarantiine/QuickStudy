f836fc044e4d70fa7012014c51090ccd953c71b0
Daniel Ward <50274655+Quarantiine@users.noreply.github.com>
2024-05-15 03:04:43 -0500

    sm change in mobile image adding

--- src/components/Dashboard/Notes/SectionNote.jsx
+++ src/components/Dashboard/Notes/SectionNote.jsx
@@ -212,6 +212,7 @@ export default function SectionNote({ folder, sectionNote }) {
 														) : (
 															<>
 																<div className="bg-gray-300 w-full h-full rounded-lg flex justify-center items-center text-sm text-gray-500 text-center text-btn p-1">
+																	<input {...getInputProps()} />
 																	<p>Upload Image</p>
 																</div>
 															</>
